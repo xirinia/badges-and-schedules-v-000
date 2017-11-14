aedc97b62270151eef1a2c8b4e00c36c94a3f6ee
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 04:26:36 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -20,7 +20,7 @@ def assign_rooms(attendees)
   rooms
 end
 
-def printer(batch_badge_creator)
+def printer(batch_badge_creator(attendees))
   batch_badge_creator.each do |badge|
     puts "#{badge}"
   end
