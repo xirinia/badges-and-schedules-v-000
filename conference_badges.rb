1cc5f0352b74b0d5335cc9f9cbc206993096c0d4
Anaxirinia Jimenez <anax131@gmail.com>
2017-11-14 04:04:53 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,8 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz" ]
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  attendees.each do |badges|
+    "Hello, my name is #{badges}."
+end
